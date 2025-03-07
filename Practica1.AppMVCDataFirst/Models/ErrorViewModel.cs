991d693de4a780225ef8d3fd97a657c3d50a902a
MINEDUCYT <MINEDUCYT@DESKTOP-CC30LNJ>
2025-03-06 20:14:05 -0600

    Agregar archivos de proyecto.

---
+++ Practica1.AppMVCDataFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica1.AppMVCDataFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
